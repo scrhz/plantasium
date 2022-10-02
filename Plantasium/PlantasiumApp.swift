1799b6ade4b9a7bf4f54c937809af79bdb8af6fd
Oscar Hesmondhalgh <oscar.hesmondhalgh@focusrite.com>
2022-10-02 19:08:39 +0100

    Initial Commit

---
+++ Plantasium/PlantasiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantasiumApp.swift
+//  Plantasium
+//
+//  Created by Oscar Hesmondhalgh on 02/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PlantasiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
