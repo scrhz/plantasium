3a9ff8c433db66680a56c6095d76f65fc95f83f4
Oscar Hesmondhalgh <oscar.hesmondhalgh@focusrite.com>
2024-02-18 13:56:47 +0000

    Add test skeletons for Notification Centre

--- PlantasiumTests/NotificationCentreTests.swift
+++ PlantasiumTests/NotificationCentreTests.swift
@@ -1,8 +1,26 @@
-//
-//  NotificationCentreTests.swift
-//  PlantasiumTests
-//
-//  Created by Oscar Hesmondhalgh on 18/02/2024.
-//
+import XCTest
+@testable import Plantasium
 
-import Foundation
+class NotificationCentreTests: TestCase {
+    func testNotificationCentreInitialisesProperly() {
+        let notificationCentre = NotificationCentre()
+
+        XCTAssertNotNil(notificationCentre)
+    }
+
+    func testNotificationCentrePostsNotificationForGivenPlantTime() {
+        let plant = Plant(name: "test plant", feedPeriod: .fortnight, lastFeed: Date.now)
+
+        let notificationCentre = NotificationCentre()
+
+        notificationCentre.scheduleNotification(for: plant)
+
+        let plants: [Plant] = []
+
+        XCTAssertEqual(notificationCentre.scheduledNotifications.count, 1)
+        TestUtils.assertDatesAreEqual(
+            date1: notificationCentre.scheduledNotifications.first.scheduledDate,
+            date2: Date(timeIntervalSinceNow: TimeInterval(14 * TestUtils.oneDay))
+        )
+    }
+}
